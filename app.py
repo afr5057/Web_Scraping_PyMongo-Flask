5828e07eaf5f9869689675facc9966f87ca63eb6
AFR5057 <afr5057@gmail.com>
2019-05-16 22:50:13 -0500

    attempt create static flask

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 # Import Dependencies 
 from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
+# from flask_flatpages import FlatPages
+# need to research: https://nicolas.perriault.net/code/2012/dead-easy-yet-powerful-static-website-generator-with-flask/
+# https://stevenloria.com/hosting-static-flask-sites-on-github-pages/
 import scrape_mars
 import os
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
@@ -29,4 +36,4 @@ def newhome():
     return render_template("index.html", mars_info=mars_info)
 
 if __name__ == "__main__": 
-    app.run(debug= True)
+    app.run(port=8000)
